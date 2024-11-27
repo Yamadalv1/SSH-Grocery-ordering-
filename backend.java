a7246081b4e40488bcd9a095ed7a1199f9cc9a08
Ahmed Alam <ahmedalam@MacBookPro.mynet>
2024-11-27 23:18:54 +0000

    Add backend.java

---
+++ backend.java
@@ -0,0 +1,13 @@
+public class backend 
+{
+    public static void main(String[] args) 
+    {
+        testing();
+    }
+    
+    public static void testing()
+    {
+        System.out.println("Testing 123!");
+    }
+
+}
