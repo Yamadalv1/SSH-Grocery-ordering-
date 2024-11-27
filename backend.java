484e6a7c0b46f04485febaf396ba6df3a0c5d601
Ahmed Alam <ahmedalam@MacBookPro.mynet>
2024-11-27 23:55:15 +0000

    Add example mock data

--- backend.java
+++ backend.java
@@ -1,13 +1,22 @@
+
 public class backend 
 {
     public static void main(String[] args) 
     {
-        testing();
+        
     }
     
-    public static void testing()
+    public static void mockData()
     {
-        System.out.println("Testing 123!");
+        String[][] products = new String[3][2];
+        products[0][0] = "Milk";
+        products[0][1] = "1.0";
+        products[1][0] = "Bread";
+        products[1][1] = "2.0";
+        products[2][0] = "Chicken";
+        products[2][1] = "5.0";    
     }
+
+    
 
 }
