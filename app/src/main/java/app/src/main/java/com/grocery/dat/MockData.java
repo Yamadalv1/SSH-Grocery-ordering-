b9b7bfb16ee035a289a25060199da96075cfbc46
Ahmed Alam <ahmedalam@MacBookPro.mynet>
2024-12-10 01:21:51 +0000

    Add files MockData.java, Order.java, OrderItem.java, Product.java in correct package

---
+++ app/src/main/java/app/src/main/java/com/grocery/dat/MockData.java
@@ -0,0 +1,25 @@
+package app.src.main.java.com.grocery.dat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockData {
+    private final List<Product> productCatalog = new ArrayList<>();
+    private final List<User> usersList = new ArrayList<>(); 
+
+    public List<Product> getProductCatalog(){
+        return productCatalog;
+    }
+
+    public void setProductCatalog(Product products){
+        productCatalog.add(products);
+    }
+
+    public List<User> getUsers(){
+        return usersList; 
+    }
+
+    public void setUsers(User users){
+        usersList.add(users); 
+    }
+} 
---
+++ app/src/main/java/app/src/main/java/com/grocery/dat/OrderItem.java
@@ -0,0 +1,50 @@
+package app.src.main.java.com.grocery.dat;
+
+public class OrderItem {
+    private int productId;
+    private String productName;
+    private int quantity;
+    private double productPrice;
+    private double totalProductPrice;
+
+    public int getProductId(){
+        return productId;
+    }
+
+    public void setProductId(int productId){
+        this.productId = productId;
+    }
+
+    public String getProductName(){
+        return productName;
+    }
+
+    public void setProductName(String productName){
+        this.productName = productName;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public double getProductPrice(){
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice){
+        this.productPrice = productPrice;
+    }
+
+    public double getTotalProductPrice(){
+        return totalProductPrice;
+    }
+
+    public void setTotalProductPrice(double productPrice, int quantity){
+        this.totalProductPrice = productPrice * quantity;
+    }
+    
+}
