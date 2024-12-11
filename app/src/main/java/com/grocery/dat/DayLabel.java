65ef3767cbc1cba2cc20b61683850d99ce798de4
Mohamed Khashogji <Mxk506@student.bham.ac.uk>
2024-12-11 23:53:14 +0300

    Modified overall project structure, updated the build.gradle file

---
+++ app/src/main/java/com/grocery/dat/DayLabel.java
@@ -0,0 +1,24 @@
+package app.src.main.java.com.grocery.dat;
+
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class DayLabel extends JLabel {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DayLabel(String text, Color background, Color foreground, boolean btn) {
+		setText(text);
+		setHorizontalAlignment(JLabel.CENTER);
+		setFont(new Font("Helvetica", Font.PLAIN, 20));
+		setOpaque(true);
+		setBackground(background);
+		setForeground(foreground);
+		if (btn) setCursor(new Cursor(Cursor.HAND_CURSOR));
+	}
+	
+}
